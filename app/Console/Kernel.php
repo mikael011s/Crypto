cfd1c24e69a6df8d25718c5921f9dd6be4d42122
Murad Gapurovich <darkkukri0550@gmail.com>
2022-08-12 07:33:16 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->call([RauCoinController::class, 'updateRate'])->everyMinute();
+        $schedule->call([RauCoinController::class, 'updateRate'])->hourly();
     }
 
     /**
